756709a4ee5a25e8600f5bea1c30dc6745bf1641
ChrisLamanu <christopher.vrel@gmail.com>
2023-05-09 22:47:05 +0200

    Adding router + mixin

---
+++ src/Router.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import Error from "./pages/Error/Error";
+import Property from "./pages/Property/Property";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/property/:id" element={<Property/>} />
+            <Route path="*" element={<Error state={{ status : 404 }} />}/>
+        </Routes>
+    )
+}
+  
+export default Router;
